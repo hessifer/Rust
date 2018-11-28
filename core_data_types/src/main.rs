cdff5c0d6ad3acbd7350c8b7919e6152911fac8c
Charles A. Hessifer <hessifer@gmail.com>
2018-11-28 16:39:23 -0500

    initial

--- core_data_types/src/main.rs
+++ core_data_types/src/main.rs
@@ -2,6 +2,10 @@
 use std::mem;
 
 fn main() {
+    // INTEGER
+
+    // valid integer types i8 u8 i16 u16 i32 u32 i64 u64
+    // isize or usize (get you the size of the pointer)
     let a:u8 = 123; // 8 bit unsigned integer
     println!("a = {}", a); // interpolation
 
@@ -13,4 +17,9 @@ fn main() {
     println!("b = {}", b);
     // use mem::size_of_val() to display size in bytes, below should be 1 byte give 8 bit integer
     println!("the size of b in bytes is {}", mem::size_of_val(&b));
-}
+
+    // CHAR
+    let d:char = 'x'; // largest unicode size (4 bytes)
+    println!("Your char is {}", d);
+
+    // ISIZE / USIZE (depends on your arch) let z:isize = 123; // variable z size is the same as the memory address for this specific OS let size_of_z = mem::size_of_val(&z); println!("z = {}, takes up {} bytes, {}-bit os", z, size_of_z, size_of_z * 8); // float let e:f64 = 2.5; // double-precision, 8 bytes or 64 bits, f64 println!("e = {}, size = {} bytes", e, mem::size_of_val(&e)); let f:f32 = 2.5; // double-precision, 8 bytes or 64 bits, f32 println!("f = {}, size = {} bytes", f, mem::size_of_val(&f)); }
