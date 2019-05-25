bfe74de99293921890bd05913a3cc29a6b6b2c3b
Charles A. Hessifer <hessifer@gmail.com>
2019-05-24 23:35:47 -0400

    added find_odds to work with vectors of type i32

--- core_data_types/src/arrays.rs
+++ core_data_types/src/arrays.rs
@@ -6,3 +6,16 @@ pub fn find_evens(nums: &[i32]) {
         }
     }
 }
+
+pub fn find_odds(nums: &[i32]) -> Vec<i32> {
+    // takes a ref to an array of i32 and returns a vector of odd numbers
+    let mut results: Vec<i32> = Vec::new();
+
+    for num in nums.iter() {
+        if num %2 != 0 {
+            results.push(*num);
+        }
+    }
+    // println!("Odds: {:?}", results);
+    results
+}
