69592601940c330f8783fe1c6ecd9deccae7fe42
Charles A. Hessifer <hessifer@gmail.com>
2019-07-21 13:00:00 -0400

    adding example of struct declaration and implementation

--- rust_learning_lab/working_with_structs/src/main.rs
+++ rust_learning_lab/working_with_structs/src/main.rs
@@ -1,3 +1,25 @@
+struct User {
+    name: String,
+    age:  i32,
+    height: i32,
+    weight: i32,
+    shoesize: i32,
+}
+
+impl User {
+    fn simple_string(&self) -> String {
+        format!("User: {}\nAge: {}\nHeight: {}\"\nWeight: {} lbs\nShoe Size: {}\n\n", self.name, self.age, self.height, self.weight, self.shoesize)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let u = User {
+        name: "Charles".to_string(),
+        age: 45,
+        height: 68,
+        weight: 175,
+        shoesize: 9,
+    };
+
+    println!("User is {}", u.simple_string());
 }
