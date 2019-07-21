8e105ae6fc670536135794fcd3b2f2d9b46308eb
Charles A. Hessifer <hessifer@gmail.com>
2019-07-21 15:06:30 -0400

    added two new functions as examples

--- rust_learning_lab/working_with_structs/src/main.rs
+++ rust_learning_lab/working_with_structs/src/main.rs
@@ -11,10 +11,18 @@ impl User {
         format!("User: {}\nAge: {}\nHeight: {}\"\nWeight: {} lbs\nShoe Size: {}\n\n", self.name,
                 self.age, self.height, self.weight, self.shoesize)
     }
+
+    fn grow(&mut self, h: i32) {
+        self.height += h;
+    }
+
+    fn die(self) {
+        println!("Dead {} ", self.simple_string());
+    }
 }
 
 fn main() {
-    let u = User {
+    let mut u = User {
         name: "Charles".to_string(),
         age: 45,
         height: 68,
@@ -23,4 +31,9 @@ fn main() {
     };
 
     println!("{}", u.simple_string());
+    u.grow(3);
+    println!("{}", u.simple_string());
+
+    u.die(); // u is now consumed and can no longer be used
+    // u.grow(10); // will error as 'u' has moved
 }
