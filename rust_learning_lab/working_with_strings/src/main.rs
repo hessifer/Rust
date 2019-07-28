079bac227b852d6e1c0c498f85eafc311643ede3
Charles A. Hessifer <hessifer@gmail.com>
2019-07-28 15:15:45 +0000

    fixed println!() issue and ran cargo fmt

--- rust_learning_lab/working_with_strings/src/main.rs
+++ rust_learning_lab/working_with_strings/src/main.rs
@@ -1,30 +1,36 @@
 fn main() {
     let last_name = String::from("Charles"); // string array owns the data, mutable
-    
-    let first_name = "bob";  // ptr to data, no ownership and immutable
-    
+
+    let first_name = "bob"; // ptr to data, no ownership and immutable
+
     for i in first_name.chars() {
         print!("{} ", i.to_lowercase());
     }
     println!("\n");
-    
+
     for c in last_name.chars() {
         print!("{} ", c.to_lowercase());
     }
     println!();
     println!("Size of Charles in bytes: {}", last_name.len());
-    
-    for (i, c) in last_name.char_indices() { // can be used instead of .chars().enumerate()
+
+    for (i, c) in last_name.char_indices() {
+        // can be used instead of .chars().enumerate()
         println!("{} = {}", i, c);
     }
     println!();
-    println!("{} has {} vowels in it.", last_name, count_vowels(&last_name));
+    println!(
+        "{} has {} vowels in it.",
+        last_name,
+        count_vowels(&last_name)
+    );
 }
 
-fn count_vowels(s: &str) -> i32 {  // &str is more flexible here
+fn count_vowels(s: &str) -> i32 {
+    // &str is more flexible here
     let vowels = vec!['a', 'e', 'i', 'o', 'u'];
     let mut result = 0;
-    
+
     for c in s.chars() {
         if vowels.contains(&c) {
             result += 1;
