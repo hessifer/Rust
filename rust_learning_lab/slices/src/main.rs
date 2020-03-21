e5f127b645f7fd7add30218cd883ac91c81a6da4
Charles A. Hessifer <hessifer@gmail.com>
2020-03-21 13:47:53 -0400

    modified function to return Vec<&str>

--- rust_learning_lab/slices/src/main.rs
+++ rust_learning_lab/slices/src/main.rs
@@ -1,35 +1,39 @@
-fn first_word(s: &String) -> &str {
+fn get_words(s: &String) -> Vec<&str> {
     let bytes = s.as_bytes();
     let mut words = Vec::new();
-    let mut offset = 0;
+    let mut pos = 0;
 
     for (i, &item) in bytes.iter().enumerate() {
         // capture words but ignore blank spaces
         if item == b' ' {
-            if offset == 0 {
-                words.push(&s[offset..i]);
-                offset = i;
+            if pos == 0 {
+                words.push(&s[pos..i]);
+                pos = i;
             } else {
-                words.push(&s[offset + 1..i]);
-                offset = i;
+                words.push(&s[pos + 1..i]);
+                pos = i;
             }
         }
     }
 
     // capture last word
-    if offset > 0 {
-        words.push(&s[offset..bytes.len()].trim());
+    if pos > 0 {
+        if pos != &s.len() - 1 {
+            // words.push(&s[pos..bytes.len()].trim());
+            words.push(&s[pos + 1..bytes.len()]);
+        }
     }
 
     println!("Number of words: {}", words.len());
     println!("Words: {:?}", words);
-    words[0]
+    words
 }
 
 fn main() {
-    let s = String::from("All Work & No Play");
-    let word = first_word(&s);
+    let s = String::from("All Work & No Play ");
+    let words = get_words(&s);
     // s.clear(); // empties string
 
-    println!("First Word: {}", word);
+    println!("First Word: {}", words[0]);
+    println!("Last Word: {}", words[words.len() - 1]);
 }
