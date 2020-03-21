5419d1ac4f22621b6431a883d829b53c74721acd
Charles A. Hessifer <hessifer@gmail.com>
2020-03-21 16:11:09 -0400

    adding student enrollment

--- rust_learning_lab/working_with_structs/src/main.rs
+++ rust_learning_lab/working_with_structs/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 struct User {
     name: String,
     age: i32,
@@ -23,6 +25,29 @@ impl User {
     }
 }
 
+// Student
+struct Student {
+    first_name: String,
+    last_name: String,
+    student_id: usize,
+    courses: Vec<String>,
+}
+
+impl Student {
+    fn add_course(&mut self, course: &str) {
+        self.courses.push(course.to_string());
+    }
+/*
+    fn remove_course(&mut self) {
+
+    }
+
+    fn list_courses(&self) {
+
+    }
+*/
+}
+
 fn main() {
     let mut u = User {
         name: "Charles".to_string(),
@@ -38,4 +63,27 @@ fn main() {
 
     u.die(); // u is now consumed and can no longer be used
              // u.grow(10); // will error as 'u' has moved
+
+
+    // Student Example
+    let mut charles = Student {
+        first_name: "Charles".to_string(),
+        last_name: "Hessifer".to_string(),
+        student_id: 000001,
+        courses: Vec::new(),
+    };
+
+    // Add course
+    let mut input = String::new();
+    println!("What course would you like to enroll in?");
+    
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => charles.add_course(&input),
+        Err(_) => println!("Unable to enroll in course: {}", input),
+    }
+    println!("Student ID: {}\n\tName: {} {}", charles.student_id, charles.first_name, charles.last_name);
+    println!("\tCourses:");
+    for course in charles.courses {
+        println!("\t\t{}", course);
+    }
 }
