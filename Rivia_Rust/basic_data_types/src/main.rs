e9cd4bab69a796a74a9e37be5e86531b20f7d080
Charles A. Hessifer <hessifer@gmail.com>
2020-03-29 19:27:19 -0400

    initial examples

--- Rivia_Rust/basic_data_types/src/main.rs
+++ Rivia_Rust/basic_data_types/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // variable declarations
+    let my_age: u8 = 27;
+    let first_letter_of_name: char = 'c';
+    let interest_rate: f64 = 3.96;
+    let message: String = String::from("Hello!");
+    let colors: [&str; 3] = ["reg", "green", "blue"];
+    let data: (u8, f64, String) = (my_age, interest_rate, message);
+
+    println!("Age: {}", my_age);
+    println!("Char: {}", first_letter_of_name);
+    println!("First Color: {}", colors[0]);
+    println!("Age: {} - Interest: {} - Message: {}", data.0, data.1, data.2);
+
+    // Destructure tuple example
+    let (x, y, z) = data;
+    println!("{}, {}, {}", x, y, z);
 }
