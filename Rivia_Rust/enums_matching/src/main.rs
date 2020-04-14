f1ad5942a40e7a1c90de41ab987a7f764bcebd8d
Charles A. Hessifer <hessifer@gmail.com>
2020-04-13 22:26:03 -0400

    coding examples of match, if let, if let else

--- Rivia_Rust/enums_matching/src/main.rs
+++ Rivia_Rust/enums_matching/src/main.rs
@@ -1,3 +1,51 @@
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Size {
+    Small,
+    Medium,
+    Large,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Desert {
+    Cake,
+    IceCream(Size),
+    Candy,
+    MilkShake(Size),
+    Pie,
+}
+
+#[derive(Debug)]
+struct Customer {
+    name: String,
+    ticket_number: i32,
+    order_time: String,
+    is_member: bool,
+    order: Desert,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let customer_01 = Customer {
+        name: String::from("Charles"),
+        ticket_number: 465,
+        order_time: String::from("16:43"),
+        is_member: false,
+        order: Desert::MilkShake(Size::Medium),
+    };
+
+    if let Desert::MilkShake(size) = customer_01.order {
+        println!("{}, I hope you enjoy your {:?} Milk Shake!", customer_01.name, size);
+    } else {
+        println!("{}, enjoy your meal.", customer_01.name);
+    }
+
+    if customer_01.is_member {
+        println!("Thanks for being a value savings member!")
+    } else {
+        println!("Would you like to become a member and save 10% today?");
+    }
+
+    print!("Order Details\nTicket Number: {}\nOrder Time: {}\n", customer_01.ticket_number,
+            customer_01.order_time);
 }
