cd1990d4fc69100b5f7822f997640b47a46d0267
Charles A. Hessifer <hessifer@gmail.com>
2021-12-08 20:38:07 -0500

    using enumerate in line iteration

--- simple_search/src/main.rs
+++ simple_search/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let search_term = "linux";
+    let quote = "\
+    Linux, careful you might learn something and
+    all work and no play
+    git bit by byte linux is hype";
+
+    for (i, line) in quote.to_lowercase().lines().enumerate() {
+        if line.contains(search_term) {
+            println!("Line {}: {}", i, line);
+        }
+    }
 }
