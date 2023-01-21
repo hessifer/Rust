d5509e6c42025e9defee573e4c9f7f2b7ebb7fb4
Charles A. Hessifer <hessifer@gmail.com>
2023-01-21 14:13:09 -0600

    flappy game menu and window

--- Gaming/flappy/src/main.rs
+++ Gaming/flappy/src/main.rs
@@ -25,6 +25,29 @@ impl State {
         ctx.print_centered(5,    "Welcome    to     Flappy     Dragon");
         ctx.print_centered(8,     "(P)  Play Game");
         ctx.print_centered(9,     "(Q)  Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
 
