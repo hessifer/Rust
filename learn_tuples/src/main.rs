4c154e7092f904e9b2765b8e56db7d97a6698357
Charles A. Hessifer <hessifer@gmail.com>
2024-12-24 20:25:49 -0500

    updated code for learning tuples and removed lifetimes to follow Rust elision rules.

--- learn_tuples/src/main.rs
+++ learn_tuples/src/main.rs
@@ -2,12 +2,11 @@ fn main() {
     let numbers_tuple: (i32, i32, i32) = (10, 13, 5);
     let (x, y, z) = numbers_tuple;
 
-    println!("x -> {}", x );
-    println!("y -> {}", y );
-    println!("z -> {}", z );
+    println!("x -> {}", x);
+    println!("y -> {}", y);
+    println!("z -> {}", z);
 
-    for num in numbers_tuple.iter() {
+    for num in [numbers_tuple.0, numbers_tuple.1, numbers_tuple.2].iter() {
         println!("{}", num);
     }
-
 }
--- lifetime_example1/src/main.rs
+++ lifetime_example1/src/main.rs
@@ -1,11 +1,17 @@
-fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
+fn add_with_lifetimes(i: &i32, j: &i32) -> i32 {
     *i + *j
+}
+
+fn mul_with_lifetimes(x: &i32, y: &i32) -> i32 {
+    *x * *y
 }
 
 fn main() {
     let a = 10;
     let b = 20;
     let res = add_with_lifetimes(&a, &b);
+    let mul_res = mul_with_lifetimes(&a, &b);
 
     println!("{}", res);
+    println!("{} * {} = {}", a, b, mul_res);
 }
